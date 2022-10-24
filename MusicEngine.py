b31f2e489ffda4fcaff8d918092ef982ad228bf5
Rajat Sethi <rajatsethi2001@gmail.com>
2022-10-24 02:02:22 -0400

    Switched to TD3 (from PPO)

--- MusicEngine.py
+++ MusicEngine.py
@@ -1,26 +1,44 @@
-import os
+import numpy as np
 import torch
 from MusicEnv import MusicEnv
 from os.path import exists
-from stable_baselines3 import PPO
+from stable_baselines3 import PPO, TD3
+from stable_baselines3.common.noise import NormalActionNoise
 
 env = MusicEnv()
-model_name = "MusicModel.zip"
+model_name = "MusicModelTD3.zip"
+buffer_size = 100000
+n_timesteps = 1
+learning_starts = 32
+train_freq = 4
+gradient_steps = 4
+batch_size = 1
+
+n_actions = env.action_space.shape[-1]
+action_noise = NormalActionNoise(mean=np.zeros(n_actions), sigma=0.1 * np.ones(n_actions))
+
+policy_kwargs = dict(net_arch=[64, 64])
+
+model = TD3(
+    'MlpPolicy',
+    env,
+    buffer_size=buffer_size,
+    learning_starts=learning_starts,
+    batch_size=batch_size,
+    train_freq=train_freq,
+    gradient_steps=gradient_steps,
+    action_noise=action_noise,
+    policy_kwargs=policy_kwargs,
+    verbose=1)
 
 if (exists(f"{model_name}")):
-    model = PPO.load(model_name, env=env)
-else:
-    policy_kwargs = dict(
-        activation_fn=torch.nn.ReLU,
-        net_arch=[64, 64, 64]
-    )
-    model = PPO('MlpPolicy', env, n_steps=8, batch_size=4, policy_kwargs=policy_kwargs)
+    model.set_parameters(model_name)
 
 done = False
 
 while not done:
-    model.learn(1)
-    model.save(f"{model_name}")
+    model.learn(n_timesteps)
+    model.save(model_name)
     train_more = input("Continue training? [Y/N]: ")
     if not (train_more in "yY"):
         done = True
